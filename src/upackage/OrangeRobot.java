adbc225b555aada5b3405aa1a1cba53266761965
Uzair <orangeball39@gmail.com>
2017-12-10 15:30:35 -0500

    Radar tracking, added execute

--- src/upackage/OrangeRobot.java
+++ src/upackage/OrangeRobot.java
@@ -1,123 +1,140 @@
 package upackage;
+
 //import javafx.scene.paint.Color;
 import robocode.*;
 import robocode.util.Utils;
 
 public class OrangeRobot extends AdvancedRobot {
-	
+
 	public void run() {
-			
+
 		setAdjustGunForRobotTurn(true);
 		setAdjustRadarForGunTurn(true);
 		setAdjustRadarForRobotTurn(true);
-		
+
 		setBodyColor(java.awt.Color.orange);
 		setGunColor(java.awt.Color.red);
 		setRadarColor(java.awt.Color.orange);
 		setBulletColor(java.awt.Color.lightGray);
 		setScanColor(java.awt.Color.black);
-		
-		while(true) {
-						turnRadarRight(45);
+
+		// execute c
+		while (true) {
+			setTurnRadarRight(Rules.RADAR_TURN_RATE);
+			// isRadarFrame = true;
+			System.out.println("I am in  run method: " + isRadarFrame);
+			execute();
 		}
 	}
 
-	
+	boolean isRadarFrame = false;
+
 	public void onScannedRobot(ScannedRobotEvent e) {
-	
-		double firePower= 1;
-		
+
+		double firePower = 1;
+
 		double actualBearing = mod(e.getBearing() + getHeading(), 360);
-		System.out.println("my mod="+ actualBearing);
-				
+		System.out.println("my mod=" + actualBearing);
+
 		double degreesO = findO(actualBearing, e.getHeading());
 		double L = e.getDistance();
 		double v = e.getVelocity();
-		double s = 20-(3*firePower);
-		double possibleAs [] = findPossibleA(convertToRadians(degreesO), L, v, s);
-		double realA = possibleAs[ findA(degreesO) ];
+		double s = 20 - (3 * firePower);
+		double possibleAs[] = findPossibleA(convertToRadians(degreesO), L, v, s);
+		double realA = possibleAs[findA(degreesO)];
 		double shootAngle = convertToDegrees(findShootAngle(realA, L, convertToRadians(degreesO)));
-		
+		double absoluteShootAngle = shootAngle + getGunHeading();
+
 		double radarInitialTurn = Utils.normalRelativeAngleDegrees(actualBearing - getRadarHeading());
-		double extraTurn = Math.min( Math.atan( 500 / e.getDistance() ), Rules.GUN_TURN_RATE_RADIANS );
-		double radarTurn;
-		
-		if  (radarInitialTurn < 0) {
-			radarTurn = radarInitialTurn - extraTurn;
-		} else if (radarInitialTurn > 0) {
-			radarTurn = radarInitialTurn + extraTurn;
-		} else {
-			radarTurn = extraTurn;
+		double extraTurn = convertToDegrees(
+				Math.min((Math.atan(5 / e.getDistance())) * 1, Rules.RADAR_TURN_RATE_RADIANS));
+		double radarTurn = findRadarTurn(radarInitialTurn, extraTurn);
+
+		System.out.println("ExtraTurn: " + extraTurn);
+		System.out.println("Initial" + radarInitialTurn);
+		System.out.println("radarTurn: " + radarTurn);
+
+		setTurnRadarRight(radarTurn);
+
+		if (Utils.normalRelativeAngleDegrees(absoluteShootAngle - getGunHeading()) > 0) {
+			setTurnGunRight(
+					Math.min(Utils.normalRelativeAngleDegrees(absoluteShootAngle - getGunHeading()), Rules.GUN_TURN_RATE));
+		} else if (Utils.normalRelativeAngleDegrees(absoluteShootAngle - getGunHeading()) < 0) {
+			setTurnGunLeft(
+					Math.min(Utils.normalRelativeAngleDegrees(absoluteShootAngle - getGunHeading()), Rules.GUN_TURN_RATE));
 		}
-		
-		turnGunRight(radarTurn);
-		
-print();
-	}
 
+		if (((absoluteShootAngle + 10) >= getGunHeading()) && (getGunHeading() >= (absoluteShootAngle - 10))) {
+			System.out.println("Aim");
+		} else {
+			System.out.println("Angle needed: " + (absoluteShootAngle - getGunHeading()));
+		}
 
-	private void print() {
-		//		System.out.println("shooting angle: " + convertToDegrees(findShootAngle(realA, L, convertToRadians(degreesO))));
-//		System.out.println("O radians " + convertToRadians(degreesO));
-//		System.out.println("actual a" + realA);
-//		System.out.println("s: "+s + " v: "+v + " L: "+L + " O deg: "+degreesO);
+		// isRadarFrame = !isRadarFrame;
+
+		System.out.println(isRadarFrame);
 		System.out.println();
+		System.out.println("=~=~=~=~=~=~=~=~=~=~=~=~=~=");
+		System.out.println();
+		execute();
 	}
-	
+
+	public double findRadarTurn(double radarInitialTurn, double extraTurn) {
+		if (radarInitialTurn < 0) {
+			return radarInitialTurn - extraTurn;
+		} else if (radarInitialTurn > 0) {
+			return radarInitialTurn + extraTurn;
+		} else {
+			return extraTurn;
+		}
+	}
+
 	private double mod(double x, double n) {
 		double r = x % n;
 		if (r < 0) {
-		    r += n;
+			r += n;
 		}
 		return r;
 	}
 
-
 	private double findShootAngle(double a, double L, double O) {
-		return Math.atan( (a*Math.tan(O)) / (L+a) );
+		return Math.atan((a * Math.tan(O)) / (L + a));
 	}
 
-	private double findO (double Bearing, double Heading) {
-		
+	private double findO(double Bearing, double Heading) {
+
 		if (((Bearing - Heading) % 360) >= 0) {
 			return (Bearing - Heading) % 360;
 		} else {
 			return ((Bearing - Heading) % 360) + 360;
 		}
 	}
-	private double [] findPossibleA (double O, double L, double v, double s) {
-		double quadratica = (((s*s)/(v*v))-1)/(Math.cos(O)*Math.cos(O));
-		double quadraticb = 2*L*-1;
-		double quadraticc = L*L*-1;
+
+	private double[] findPossibleA(double O, double L, double v, double s) {
+		double quadratica = (((s * s) / (v * v)) - 1) / (Math.cos(O) * Math.cos(O));
+		double quadraticb = 2 * L * -1;
+		double quadraticc = L * L * -1;
 		return solve(quadratica, quadraticb, quadraticc);
 	}
-		
-	private int findA (double degO) {
-		if (90 <= degO && degO <= 270) {
-			if (degO == 90 || degO == 270) {
-				return 0; // 0 or 1
-			} else {
-				return 0;
-			}
-		} else {
-			return 1;
-		}
-	}
-	
-	private double convertToRadians (double degrees) {
-		return (Math.PI*degrees)/180;
-	}
-	
-	private double convertToDegrees (double radians) {
-		return (radians*180)/Math.PI;
+
+	private int findA(double degO) {
+		return (90 <= degO && degO <= 270) ? 0 : 1;
 	}
 
-	private double [] solve (double a, double b, double c) {
-		
-		double [] roots = new double [2];
-		double disc = Math.sqrt((b*b)-(4*a*c));
-		roots[0] = ((-1*b)-disc)/(2*a);
-		roots[1] = ((-1*b)+disc)/(2*a);
+	private double convertToRadians(double degrees) {
+		return (Math.PI * degrees) / 180;
+	}
+
+	private double convertToDegrees(double radians) {
+		return (radians * 180) / Math.PI;
+	}
+
+	private double[] solve(double a, double b, double c) {
+
+		double[] roots = new double[2];
+		double disc = Math.sqrt((b * b) - (4 * a * c));
+		roots[0] = ((-1 * b) - disc) / (2 * a);
+		roots[1] = ((-1 * b) + disc) / (2 * a);
 		return roots;
-	}	
+	}
 }
